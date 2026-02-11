1684a6fdd17062327bbe08a0626d86bcbff49d35
Oleg Cohen <oleg.cohen@shakeiq.ai>
2026-02-10 17:00:17 -0700

    Marketing website for Empower AI

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://empowertech.ai',
+  site: 'https://enpraxis.ai',
   outDir: './docs',
   trailingSlash: 'always',
   integrations: [sitemap()],
